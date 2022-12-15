5b9b4db2fe763983fac9b959db0286e877671a75
Anaf Talukder <111679954+Anaf-Talukder@users.noreply.github.com>
2022-12-15 06:46:45 -0800

    Add files via upload
    stuffs

---
+++ hae/activePage.js
@@ -0,0 +1,16 @@
+const activePage = window.location.pathname;
+const navLinks = document.querySelectorAll('nav a').forEach(link => {
+  if(link.href.includes(`${activePage}`)){
+    link.classList.add('active');
+    console.log(link);
+  }
+})
+
+var counter = 1;
+    setInterval(function(){
+      document.getElementById('radio' + counter).checked = true;
+      counter++;
+      if(counter > 4){
+        counter = 1;
+      }
+    }, 5000);
